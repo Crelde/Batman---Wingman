364d318f41ec418d19ff0776dc0f86e4b17aba7d
Kewin Pedersen <Kiw_Killer@hotmail.com>
2012-04-19 14:37:47 +0200

    Bosk JDBC

---
+++ BOSK_JDBC/src/Opg102/DbDataAccessObject.java
@@ -0,0 +1,14 @@
+package Opg102;
+
+import java.sql.SQLException;
+
+public class DbDataAccessObject {
+
+	public static void savePerson(Person person) throws SQLException {
+
+	}
+
+	public static Person loadPerson(int id) throws SQLException {
+		return null;
+	}
+}
